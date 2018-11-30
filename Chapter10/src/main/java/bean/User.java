1ed237f3d3955f83081ee97f8fab988c6ae2ce44
sss.wang <1111111@qq.com>
2018-11-30 17:13:42 +0800

    c

---
+++ Chapter10/src/main/java/bean/User.java
@@ -0,0 +1,15 @@
+package bean;
+
+import lombok.Data;
+
+/**
+ * Created by yuchen on 2018/11/30
+ */
+@Data
+public class User {
+    private String userName;
+    private String password;
+    private String name;
+    private String age;
+    private String sex;
+}
